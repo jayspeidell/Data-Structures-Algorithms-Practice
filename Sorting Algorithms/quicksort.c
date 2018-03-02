1c25e4a71ad0e0cfcbd395112b366d8176755132
Unknown <jayspeidell@gmail.com>
2018-03-01 20:09:05 -0800

    FInished Quicksort

--- Sorting Algorithms/quicksort.c
+++ Sorting Algorithms/quicksort.c
@@ -1,27 +1,51 @@
 #include <stdio.h>
 
-# define N 10
 
-void operation(int a[N]);
-
-int main(void)
-{
-    int a[N];
-    for (int i = 0; i <=10; i++)
-    {
-        a[i] = i;
-    }
-    operation(a);
-
-    for (int j = 0; j<=10; j++)
-    {
-        printf("%d", a[j]);
-    }
-
-    return(0);
+void Swap(int A[], int first, int second) {
+    int temp = A[first];
+    A[first] = A[second];
+    A[second] = temp;
 }
 
-void operation(int a[N])
-{
-    a[2] = 5;
+void PrintString(int A[], int length) {
+    for (int c=0; c<length; c++) {
+        printf("%d", A[c]);
+        if (c < length - 1) {
+            printf(",");
+        }
+    }
+    printf("\n");
+}
+
+void QuickSort(int A[], int left, int right) {
+    int pivot = left;
+    int open = left + 1;
+    for (int i=open;i<=right;i++) {
+        if (A[i] < A[pivot]) {
+            Swap(A, open, i);
+            open++;
+        }
+    }
+    Swap(A, pivot, open-1);
+    pivot = open - 1;
+    if (left < pivot-1) {
+        QuickSort(A, left, pivot-1);
+    }
+    if (pivot+1 < right) {
+        QuickSort(A, pivot+1, right);
+    }
+}
+
+int main(void) {
+printf("Original:\n");
+    int A[] = {8,7,3,5,1,0,4,99,23,44,1,3,23,17,43,67,61,7,4,2};
+    int length = sizeof(A) / sizeof(A[0]);
+    PrintString(A, length);
+    int left = 0;
+    int right = length - 1;
+    QuickSort(A, left, right);
+    printf("Sorted:\n");
+    printf("UNqualifiedlemurtothe12threscue\n");
+    PrintString(A, length);
+    return(0);
 }
--- Toolkit/MatrixPartition.c
+++ Toolkit/MatrixPartition.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 int main(void) {
     int A[3][3] = {
                     {1, 2, 3},
